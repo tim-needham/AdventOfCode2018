7a096a07351cc38227f3a927792ab5ed97af320e
MERIDIAN\tneedham <tneedham@travelrepublic.co.uk>
2019-01-25 15:46:00 +0000

    Day 9 - now with added circles.

--- AdventOfCode2018/Day9.fs
+++ AdventOfCode2018/Day9.fs
@@ -4,48 +4,68 @@ open System;
 open System.Diagnostics;
 open System.IO;
 
+type CircularList<'a> = {
+        Value : 'a; 
+        mutable Previous : CircularList<'a> option;
+        mutable Next : CircularList<'a> option
+    };;
+
 let parse (s : string) : int * int = 
     match s.Split(' ') with
     | [| n; "players;"; "last"; "marble"; "is"; "worth"; p; "points" |] -> Int32.Parse n, Int32.Parse p;
     | _ -> failwith "Invalid input for parsing";
 
-let insert (ms : int list) (p : int) (m : int) : int*int list =
-    match ms with
-    | [] -> (m, [m]);
-    | [x] -> (m, x::[m]);
-    | xs -> match ms |> List.findIndex (fun q -> p = q) with
-            | i when i + 2 = xs.Length -> (m, List.append xs [m]);
-            | i ->  (m, xs
-                        |> List.splitAt ((i+2) % xs.Length)
-                        |> (fun (a, b) -> List.append a (m::b)));
+let singleton (v : 'a) : CircularList<'a> =
+    let c = { Value = v; Previous = None; Next = None };
+    c.Previous <- Some c;
+    c.Next <- Some c;
+    c;
 
-let remove (ms : int list) (p : int) : int*int*int list =
-    let i = ms
-            |> List.findIndex (fun q -> q = p)
-            |> (fun x -> x-7)
-            |> (fun x -> if x < 0 then x + ms.Length else x);
-    
-    let a, b =  ms
-                |> List.splitAt i;
+let rec rotate (a : int) (c : CircularList<'a>) : CircularList<'a> =
+    match a, c.Previous, c.Next with
+    | 0, _, _ -> c;
+    | v, _, Some n when v > 0 -> rotate (v-1) n;
+    | v, Some p, _ when v < 0 -> rotate (v+1) p;
+    | _ -> failwith "Invalid circular list.";
 
-    let c, ds = b |> List.head, b |> List.tail;
+let insert (v : 'a) (c : CircularList<'a>) : CircularList<'a> =
+    let d = {
+                Value = v;
+                Previous = c.Previous;
+                Next = Some c
+            };
+    c.Previous.Value.Next <- Some d
+    c.Previous <- Some d;
+    d;
 
-    (c, ds |> List.head, List.append a ds);
+let pop (c : CircularList<'a>) : 'a * CircularList<'a> option =
+    match c.Previous, c.Next with
+    | Some p, Some n when p = n -> c.Value, None;
+    | Some p, Some n -> p.Next <- Some n;
+                        n.Previous <- Some p;
+                        c.Value, Some n;
+    | _, _ -> failwith "Invalid circular list";
 
-let rec play (ms : int list) (ns : (int*int) list) (n : int) (p : int) (v : int) (l : int) : (int*int) list =
-    let n' = (n+1) % ns.Length;
+let score (n : int) (s : int64) (ns : Map<int, int64>) : Map<int, int64> =
+    match Map.containsKey n ns with
+    | false ->  Map.add n s ns;
+    | true ->   let e = Map.find n ns;
+                Map.add n (e+s) ns;
+
+let rec play (c : CircularList<int>) (ns : Map<int, int64>) (n : int) (m : int) (v : int) (l : int) : Map<int, int64> =
+    let n' = (n+1) % m;
 
     match v with
-    | x when x > l -> ns;
-    | x when x > 0 && x <= l && x%23 = 0 -> let (s, p', ms') = remove ms p;
-                                            let ns' =   ns
-                                                        |> List.map (fun (q, t) -> if q = n then (q, t + s + v) else (q, t));
-                                            //printfn "%A" ms';
-                                            play ms' ns' n' p' (v+1) l;
-    | x when x <= l ->  let (p', ms') = insert ms p v;
-                        //printfn "%A" ms';
-                        play ms' ns n' p' (v+1) l;
-
+    | x when x > l -> ns
+    | x when 0 <> x && x%23 = 0 ->  match c |> rotate -7 |> pop with
+                                    | _, None -> failwith "You popped a singleton. Now what?";
+                                    | p, Some d ->  let ns' = score n (int64(p) + int64(v)) ns;
+                                                    play d ns' n' m (v+1) l;
+    | x when x <= l ->  let d = c
+                                |> rotate 2
+                                |> insert v;
+                        play d ns n' m (v+1) l;
+    
 let run (file : string, testMode : bool) =
 
     let w = new Stopwatch();
@@ -58,14 +78,16 @@ let run (file : string, testMode : bool) =
                 |> parse;
 
     let n, p = if testMode then test else input;
-    let ns = [0..n-1] |> List.map (fun p -> (p, 0));
-
-    play [] ns 0 0 0 p
+    let ns = new Map<int,int64>([]);
+    
+    play (singleton 0) ns 0 n 1 p
+    |> Map.toList
     |> List.map (fun (_, x) -> x)
     |> List.max
     |> printfn "Day 9, part 1: %d";
-
-    play [] ns 0 0 0 (p*100)
+    
+    play (singleton 0) ns 0 n 1 (p*100)
+    |> Map.toList
     |> List.map (fun (_, x) -> x)
     |> List.max
     |> printfn "Day 9, part 2: %A";
