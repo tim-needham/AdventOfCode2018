5f5d4737d9eb0fd8c36f131d176917ca1a1a6d2c
MERIDIAN\tneedham <tneedham@travelrepublic.co.uk>
2019-01-29 11:25:48 +0000

    Day 12 - Part 1 - In-place array modification for the speed-win.

--- AdventOfCode2018/Day14.fs
+++ AdventOfCode2018/Day14.fs
@@ -4,26 +4,22 @@ open System;
 open System.Diagnostics;
 open System.IO;
 
-let digits (r : int) : int array =
-    match r / 10 with
-    | 0 -> [| r |];
-    | n -> [| n; r%10 |];
-
-let brew (rs : int array) (e : int) (f : int) : int array * int * int =
+let brew (rs : int array) (e : int) (f : int) (i : int) : int array * int * int * int =
     let r = rs.[e] + rs.[f];
-    let ss = (digits r) |> Array.append rs;
-    let g, h = (e + rs.[e] + 1) % ss.Length, (f + rs.[f] + 1) % ss.Length;
-    ss, g, h;
+    let j = match r / 10 with
+            | 0 ->  Array.set rs i r;
+                    i + 1;
+            | n ->  Array.set rs i n;
+                    Array.set rs (i+1) (r%10);
+                    i + 2;
+    let g, h = (e + rs.[e] + 1) % j, (f + rs.[f] + 1) % j;
+    rs, g, h, j;
 
-let rec cook (rs : int array) (e : int) (f : int) (t : int) : int list =
-    match rs.Length with
-    | n when n >= t + 10 -> rs
-                            |> Seq.toList
-                            |> List.take (t + 10)
-                            |> List.skip t;
-    | _ ->  let ss, g, h = brew rs e f;
-            //printfn "%A, %A"  ss fs;
-            cook ss g h t;
+let rec cook (rs : int array) (e : int) (f : int) (i : int) (t : int) : int array =
+    match i with
+    | n when n >= t + 10 -> rs.[t..(t+9)];
+    | _ ->  let ss, g, h, j = brew rs e f i;
+            cook ss g h j t;
 
 let run (file : string, testMode : bool) =
 
@@ -35,8 +31,12 @@ let run (file : string, testMode : bool) =
 
     let test = 9;
 
+    let recipes = Array.zeroCreate<int> ((if testMode then test else input) + 12);
+    Array.set recipes 0 3;
+    Array.set recipes 1 7;
+
     if testMode then test else input
-    |> cook [| 3; 7 |] 0 1
+    |> cook recipes 0 1 2
     |> printfn "Day 14, part 1: %A";
 
     0
